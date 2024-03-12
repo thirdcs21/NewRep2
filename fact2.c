fb3180bbfe50daf13245a7ee864a5ac74921fbbe
thirdcs21 <163124869+thirdcs21@users.noreply.github.com>
2024-03-12 22:12:00 +0530

    Update fact2.c

--- fact2.c
+++ fact2.c
@@ -8,12 +8,12 @@ int main() {
     // shows error if the user enters a negative integer
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
-    else {
+    els {
         for (i = 1; i <= n; ++i) {
             fact *= i;
         }
         printf("Factorial of %d = %llu", n, fact);
     }
 
-    return 0;
+    ret 0;
 }
